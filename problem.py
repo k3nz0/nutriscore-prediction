cbd47c031a4bde27e47aaa76b51e1f8ba25db03c
Nicolas.D <nico.devatine@gmail.com>
2020-01-30 16:48:41 +0100

    Add business case + start data analysis

--- problem.py
+++ problem.py
@@ -4,21 +4,18 @@ import rampwf as rw
 
 # TODO add workflow : https://github.com/paris-saclay-cds/ramp-workflow/wiki/Build-your-own-workflow
 
-
 problem_title = 'Nutriscore prediction'
 
 def _read_data(path, f_name):
-    data = pd.read_csv(os.path.join(path, 'data', f_name))
-    y_array = data[_target_column_name].values
-    X_df = data.drop([_target_column_name] + _ignore_column_names, axis=1)
+    data = pd.read_csv(os.path.join(path, f_name), sep='|', compression='zip')
+    y_array = data['nutriscore_grade'].values
+    X_df = data.drop(['nutriscore_grade'], axis=1)
     return X_df, y_array
 
-
 def get_train_data(path='.'):
-    f_name = 'train.csv'
+    f_name = 'data_train.csv.zip'
     return _read_data(path, f_name)
 
-
 def get_test_data(path='.'):
-    f_name = 'test.csv'
+    f_name = 'data_test.csv.zip'
     return _read_data(path, f_name)
